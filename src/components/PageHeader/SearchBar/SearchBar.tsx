a1d0daca0dc11c922a86da6987ad2d4ad08806b9
Bogdan-Hasanov <47710368+Bogdan-Hasanov@users.noreply.github.com>
2021-06-06 19:18:29 +0300

    Added snackbar for search term validation

--- src/components/PageHeader/SearchBar/SearchBar.tsx
+++ src/components/PageHeader/SearchBar/SearchBar.tsx
@@ -1,23 +1,43 @@
 import './SearchBar.scss';
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { connect } from 'react-redux';
-import {
-  add,
-  decrement,
-  deleteResult,
-  setLoggedIn,
-  setSearchTerm,
-  setToken,
-  storeResult,
-  subtract,
-} from '../../../store/actions/Actions';
+import { setSearchTerm } from '../../../store/actions/Actions';
+import { Alert } from '@material-ui/lab';
+import { Snackbar, SnackbarCloseReason } from '@material-ui/core';
 
 const searchBar = (props: any) => {
+  const regExp = /[a-zA-Z]/g;
+
+  const [open, setOpen] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('');
+  const validateSearchTerm = (term: string): void => {
+    if (term.length < 2) {
+      setOpen(true);
+      setErrorMessage('Too short search term');
+      return;
+    }
+    if (!regExp.test(term)) {
+      setOpen(true);
+      setErrorMessage('The term must contain letters');
+      return;
+    }
+    props.setSearchTerm(term);
+  };
   const onKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      props.setSearchTerm((event.target as HTMLInputElement).value);
+      validateSearchTerm((event.target as HTMLInputElement).value);
     }
+  };
+  const snackBarCloseHandler = (event: SyntheticEvent<any, Event>, reason: SnackbarCloseReason): void => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setErrorMessage('');
+    setOpen(false);
+  };
+  const alertCloseHandler = (): void => {
+    setErrorMessage('');
   };
   return (
     <div className="SearchBar">
@@ -27,6 +47,16 @@ const searchBar = (props: any) => {
         inputProps={{ 'aria-label': 'description', style: { fontSize: 40 } }} // font size of input text
         InputLabelProps={{ style: { fontSize: 40 } }} // font size of input label
       />
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={snackBarCloseHandler}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      >
+        <Alert onClose={alertCloseHandler} severity="error">
+          {errorMessage}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
@@ -42,5 +72,4 @@ const mapDispatchToProps = (dispatch: any) => {
     setSearchTerm: (searchTerm: string) => dispatch(setSearchTerm(searchTerm)),
   };
 };
-
 export default connect(mapStateToProps, mapDispatchToProps)(searchBar);
